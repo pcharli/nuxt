d3b8123a80cacfd0fa036dd0e23b59bdaf2f2fa6
pierre <pcepegra@gmail.com>
2022-08-30 21:57:37 +0200

    accueil

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navigation">
         <ul class="nav-list">
-            <Nuxt-link to="/">Home</Nuxt-link>
+            <Nuxt-link to="/">Accueil</Nuxt-link>
             <Nuxt-link to="/contact">Contact</Nuxt-link>
         </ul>
     </nav>
